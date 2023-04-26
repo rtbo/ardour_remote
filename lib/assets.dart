70f169b54bbf87692df5c4433519b34c080e111a
Remi Thebault <remi.thebault@gmail.com>
2023-04-27 01:37:28 +0200

    assets.dart

---
+++ lib/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Assets {
+  static var icons = _Icons();
+}
+
+class _Icons {
+  final connect_ardour = "assets/icons/connect_ardour.png";
+}
