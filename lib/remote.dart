7e230a9df8cb7e5c4e9a69775b851e95f7599a07
Remi Thebault <remi.thebault@gmail.com>
2023-04-30 16:30:59 +0200

    remote error card and wait indicator

--- lib/remote.dart
+++ lib/remote.dart
@@ -1,7 +1,8 @@
-import 'package:ardour_remote/model/remote.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'assets.dart';
+import 'model/ardour_remote.dart';
 import 'model/connection.dart';
 
 class RemotePage extends StatelessWidget {
@@ -44,25 +45,121 @@ class _RemoteScreenState extends State<RemoteScreen> {
   @override
   Widget build(BuildContext context) {
     final remote = context.watch<ArdourRemote>();
+    final theme = Theme.of(context);
     final conn = widget.connection;
     Widget body;
     if (remote.connected) {
       body = Text("Connected to ${conn.toString()}");
     } else if (remote.error != null) {
-      body = Text("Error: ${remote.error}");
+      return RemoteError(errorText: remote.error!);
     } else {
-      body = const Text("Connecting...");
+      return const WaitConnection();
     }
+
+    final colorAppBar = theme.colorScheme.primary;
+    final colorOnAppBar = theme.colorScheme.onPrimary;
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
+          backgroundColor: colorAppBar,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: colorOnAppBar,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Row(
+            children: [
+              Image.asset(Assets.icons.connect_ardour, color: colorOnAppBar),
+              const SizedBox(width: 8),
+              Text(remote.sessionName, style: TextStyle(color: colorOnAppBar)),
+            ],
+          )),
       body: body,
     );
+  }
+}
+
+class RemoteError extends StatelessWidget {
+  final String errorText;
+
+  const RemoteError({super.key, required this.errorText});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+              child: Column(children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.error, color: theme.colorScheme.error),
+              title: const Text("Error: Can't connect to Ardour"),
+              subtitle: Text(errorText),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+              TextButton(
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("BACK TO CONNECTION SETUP"),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ])
+          ])),
+        ],
+      )),
+    );
+  }
+}
+
+class WaitConnection extends StatefulWidget {
+  const WaitConnection({super.key});
+
+  @override
+  State<WaitConnection> createState() => _WaitConnectionState();
+}
+
+class _WaitConnectionState extends State<WaitConnection>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircularProgressIndicator(
+          value: controller.value,
+          semanticsLabel: "Waiting for Ardour",
+        ),
+      ],
+    ));
   }
 }
