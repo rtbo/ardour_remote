4fbe2065429100cfa6a79c39f61742493ad25f78
Remi Thebault <remi.thebault@gmail.com>
2023-05-01 22:44:53 +0200

    remove AppState

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../model/connection.dart';
 import 'connect.dart';
-
-class AppState extends ChangeNotifier {
-  Connection? _connection;
-
-  Connection? get connection => _connection;
-
-  set connection(Connection? c) {
-    _connection = c;
-    notifyListeners();
-  }
-}
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -21,17 +8,14 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppState(),
-      child: MaterialApp(
-        title: 'Ardour Remote',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 75, 51, 180)),
-        ),
-        home: const ConnectionPage(),
+    return MaterialApp(
+      title: 'Ardour Remote',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 75, 51, 180)),
       ),
+      home: const ConnectionPage(),
     );
   }
 }
