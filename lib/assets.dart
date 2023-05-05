0a1d85f791460d61fa4b197f2beee973514d6a07
Remi Thebault <remi.thebault@gmail.com>
2023-05-05 02:40:16 +0200

    add arrow assets

--- lib/assets.dart
+++ lib/assets.dart
@@ -7,4 +7,10 @@ class Assets {
 class _Icons {
   const _Icons();
   final ardour_connect = "assets/icons/ardour_connect.png";
+  final arrow_left_double_bar = "assets/icons/arrow_left_double_bar.png";
+  final arrow_left_bar = "assets/icons/arrow_left_bar.png";
+  final arrow_left_quarter = "assets/icons/arrow_left_quarter.png";
+  final arrow_right_double_bar = "assets/icons/arrow_right_double_bar.png";
+  final arrow_right_bar = "assets/icons/arrow_right_bar.png";
+  final arrow_right_quarter = "assets/icons/arrow_right_quarter.png";
 }
