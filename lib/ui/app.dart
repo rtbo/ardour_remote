dc8e08d48330e12df4f7d5a0d4aa592f8e2e7d21
Remi Thebault <remi.thebault@gmail.com>
2023-05-05 02:37:24 +0200

    optional light/dark forcing

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -6,20 +6,26 @@ import 'connect.dart';
 const _defaultLightColorScheme = ColorScheme.light();
 const _defaultDarkColorScheme = ColorScheme.dark();
 
+const forceLight = false;
+const forceDark = false;
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      final light = lightColorScheme ?? _defaultLightColorScheme;
+      final dark = darkColorScheme ?? _defaultDarkColorScheme;
+
       return MaterialApp(
         title: 'Ardour Remote',
         theme: ThemeData(
-          colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+          colorScheme: forceDark ? dark : light,
           useMaterial3: true,
         ),
         darkTheme: ThemeData(
-          colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+          colorScheme: forceLight ? light : dark,
           useMaterial3: true,
         ),
         home: const ConnectionPage(),
