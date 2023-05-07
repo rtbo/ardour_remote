8feff36308c91a73551fb8c8640faa3ec94f88cc
Remi Thebault <remi.thebault@gmail.com>
2023-05-07 14:05:38 +0200

    gen-launcher-pngs.sh

---
+++ gen-launcher-pngs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+LAUNCHERS=("launcher" "launcher_rounded" "launcher_foreground" "launcher_background")
+LEN=${#LAUNCHERS[@]}
+
+mkdir -p "$SCRIPT_DIR/assets/launchers"
+
+for ((I=0;I<$LEN;I++));
+do
+    L=${LAUNCHERS[$I]}
+
+    echo "exporting assets/launchers/$L.png"
+
+    inkscape \
+        --export-filename="assets/launchers/$L.png" \
+        --export-area-page \
+        --export-width=1024 \
+        "artwork/$L.svg"
+
+done
